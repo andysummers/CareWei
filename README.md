36b7b5766ffa9a207b77b42aaa63513517cccfd0
Andy Summers <andysummers@shaw.ca>
2017-11-20 16:11:38 -0800

    Start

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# CareWei
+# Garden Helper Xamarin.Forms Sample app
+### Presented at Connect 2017 by Mark Smith and Adrian Stevens
+### Presentation Copyright (C) 2017 Xamarin University, Microsoft
---
+++ src/GardenHelper.Services.Azure/AzureVisionService.cs
@@ -0,0 +1,59 @@
+﻿using GardenHelper.Interfaces;
+using Microsoft.Cognitive.CustomVision;
+using Microsoft.Cognitive.CustomVision.Models;
+using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GardenHelper.Services.Azure
+{
+    public class AzureVisionService : IIdentifyPicture
+    {
+        public async Task<string> IdentifyAsync(Func<Stream> picture)
+        {
+            var description = await GetImageDescriptionAsync(picture.Invoke());
+            if (description?.Tags.Any(t => t.Name == "plant" || t.Name == "flower") != true)
+            {
+                throw new Exception("Your picture does not appear to contain a plant. Please try again.");
+            }
+
+            var hint = description.Tags.FirstOrDefault(t => t.Hint == "flower");
+            if (hint != null)
+                return hint.Name;
+
+        	var result = await PredictImageAsync(picture.Invoke());
+            var tag = result.Predictions.OrderByDescending(t => t.Probability).FirstOrDefault();
+            if (tag?.Probability < .25)
+            {
+                throw new Exception("I'm sorry, I don't recognize that plant.");
+            }
+
+            return tag?.Tag;
+        }
+
+        private async Task<ImagePredictionResultModel> PredictImageAsync(Stream stream)
+        {
+            var ep = new PredictionEndpoint(new PredictionEndpointCredentials(AuthKeys.VisionPredictionKey)) { BaseUri = new Uri(AuthKeys.VisionPredictionUrl) };
+            var response = await ep.PredictImageWithHttpMessagesAsync(AuthKeys.GardenCenterProjectId, stream).ConfigureAwait(false);
+            return response.Body;
+        }
+
+        private Task<AnalysisResult> GetImageDescriptionAsync(Stream imageStream)
+        {
+            try
+            {
+                VisualFeature[] features = { VisualFeature.Tags };
+                IVisionServiceClient visionClient = new VisionServiceClient(AuthKeys.VisionSubscriptionKey, AuthKeys.VisionUrl);
+
+                return visionClient.AnalyzeImageAsync(imageStream, features.ToList(), null);
+            }
+            catch (ClientException ex)
+            {
+                throw new Exception(ex.Error.Message, ex);
+            }
+        }
+    }
+}
